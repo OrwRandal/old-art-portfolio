6aa8464a8c3b490100eee29ae49f5b7fd26cde90
OrwRandal <randypichardo1987@gmail.com>
2023-03-04 22:27:09 -0500

    added opening image functionality

---
+++ script.js
@@ -0,0 +1,89 @@
+document.querySelector('#img1').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage1').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage1').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage1').style.display = 'none';
+})
+
+document.querySelector('#img2').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage2').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage2').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage2').style.display = 'none';
+})
+
+document.querySelector('#img3').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage3').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage3').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage3').style.display = 'none';
+})
+
+document.querySelector('#img4').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage4').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage4').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage4').style.display = 'none';
+})
+
+document.querySelector('#img5').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage5').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage5').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage5').style.display = 'none';
+})
+
+document.querySelector('#img6').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage6').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage6').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage6').style.display = 'none';
+})
+
+document.querySelector('#img7').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage7').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage7').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage7').style.display = 'none';
+})
+
+document.querySelector('#img8').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage8').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage8').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage8').style.display = 'none';
+})
+
+document.querySelector('#img9').addEventListener('click', () => {
+    if (window.innerWidth > 700) {
+        document.querySelector('#openedImage9').style.display = 'flex';
+    }
+});
+
+document.querySelector('#openedImage9').querySelector('.button').addEventListener('click', () => {
+    document.querySelector('#openedImage9').style.display = 'none';
+})
